cb6b819d702df2c43ced51f2d9d554f7c70573e8
Scott Norvell <scottlnorvell@gmail.com>
2015-10-27 22:37:13 -0400

    Update title of 'dummy' app
    (so that it doesn't get a complex...)

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = {
   included: function(app, parentAddon) {
     var target = (parentAddon || app);
 
+    // Import the styles into vendor.css
+    // TODO: make this optional?
     target.import('vendor/app.css');
   }
 };
